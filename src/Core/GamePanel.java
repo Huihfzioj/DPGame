b104d412845b8891529bee42b1c527c94727b0bf
Huihfzioj <hediljlassi64@gmail.com>
2025-11-23 21:40:11 +0100

    add player sprites and movement mechanism

--- src/Core/GamePanel.java
+++ src/Core/GamePanel.java
@@ -1,13 +1,15 @@
 package Core;
 
+import Entities.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel implements Runnable{
 
-    final int originalTileSize=16; //default size of mc and npc in the game
-    final int scale=3;
-    final int tileSize=originalTileSize*scale; //64*64 tile
+    public final static int originalTileSize=16; //default size of mc and npc in the game
+    public final static int scale=3;
+    public final static int tileSize=originalTileSize*scale; //64*64 tile
     final int maxScreenCol=16;
     final int maxScreenRow=12;
     final int screenWidth= tileSize*maxScreenCol;
@@ -15,11 +17,9 @@ public class GamePanel extends JPanel implements Runnable{
 
     final int FPS = 60;
 
-    int playerX=100;
-    int playerY=100;
-    int speed=4;
     KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this,keyHandler);
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
@@ -59,26 +59,14 @@ public class GamePanel extends JPanel implements Runnable{
 
     //function to update information for the scene such as player positions
     public void update(){
-        if (keyHandler.getUpPressed()){
-            playerY-=speed;
-        }
-        else if (keyHandler.getDownPressed()){
-            playerY+=speed;
-        }
-        else if (keyHandler.getLeftPressed()){
-            playerX-=speed;
-        }
-        else if (keyHandler.getRightPressed()){
-            playerX+=speed;
-        }
+        player.update();
     }
 
     //function to draw relevant components during the update
     public void paintComponent(Graphics graphics){
         super.paintComponent(graphics);
         Graphics2D graphics2D = (Graphics2D) graphics;
-        graphics2D.setColor(Color.white);
-        graphics2D.fillRect(playerX,playerY,tileSize,tileSize);
+        player.draw(graphics2D);
         graphics2D.dispose(); //release resources(memory) if unneeded
     }
 }
